a4f94e63c186361557efaef4dc575396f13eabc9
soulfulDreamer <kazishahhamza@outlook.com>
2024-12-15 12:06:26 +0600

    Update firebase-config.ts

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,7 +13,9 @@ const firebaseConfig = {
   storageBucket: "docsclone-2d5c0.firebasestorage.app",
   messagingSenderId: "1035938458123",
   appId: "1:1035938458123:web:16c8dfb783d5578d59b50e"
-};
+}; 
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
